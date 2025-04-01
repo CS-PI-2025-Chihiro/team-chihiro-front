404a323399a6bd64ce2eae7f4eaf56a953f06fbc
andreyjodar <andreyviniciusjodar@gmail.com>
2025-04-01 17:09:45 -0300

    feat(faq/script.js): define atalhos de acessibilidade 'Enter' e ' ' \nH02-E03

--- faq/script.js
+++ faq/script.js
@@ -10,21 +10,40 @@ document.addEventListener("DOMContentLoaded", () => {
                 const button = document.createElement("button");
                 button.classList.add("faq-question");
                 button.innerHTML = `${index + 1}. ${item.question}`;
+
+                button.setAttribute("aria-expanded", "false");
+                button.setAttribute("id", `question-${index + 1}`);
+                button.setAttribute("aria-controls", `answer-${index + 1}`);
                 
                 const answer = document.createElement("p");
                 answer.classList.add("faq-answer");
                 answer.textContent = item.answer;
 
+                answer.setAttribute("id", `answer-${index + 1}`);
+                answer.setAttribute("role", "region");
+                answer.setAttribute("aria-labelledby", `question-${index + 1}`);
+
                 button.addEventListener("click", () => {
                     const isActive = button.classList.contains("active");
-                    document.querySelectorAll(".faq-question").forEach(btn => btn.classList.remove("active"));
+                    document.querySelectorAll(".faq-question").forEach(btn => {
+                        btn.classList.remove("active");
+                        btn.setAttribute("aria-expanded", "false");
+                    });
                     document.querySelectorAll(".faq-answer").forEach(ans => ans.style.display = "none");
 
                     if (!isActive) {
                         button.classList.add("active");
+                        button.setAttribute("aria-expanded", "true");
                         answer.style.display = "block";
                     }
                 });
+
+                button.addEventListener("keydown", (event) => {
+                    if(event.key === "Enter" || event.key === " ") {
+                        event.preventDefalt();
+                        button.click();
+                    }
+                })
 
                 li.appendChild(button);
                 li.appendChild(answer);
